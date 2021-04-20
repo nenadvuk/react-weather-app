25d0be483f97c108d6944157469e145c9f345203
nenadvuk <you@example.com>
2021-04-20 20:54:27 +0200

    adding forecast component

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,41 +8,61 @@ import WeatherData from './WeatherData';
 import Error from './Error';
 import DateTime from './DateTime';
 import Footer from './Footer';
+import WeatherIcon from './WeatherIcon';
 
 const Main = () => {
   const [weather, setWeather] = useState();
   const [city, setCity] = useState();
   const [error, setError] = useState();
+  const [forecast, setForecast] = useState();
+
+
   const api_call = async (e) => {
     e.preventDefault();
     const location = e.target.elements.location.value;
     if (!location) {
-      return setError(('Please enter the city'),setWeather(null));
+      return setError('Please enter the city', setWeather(null));
     }
+    // Current weather
     const API_KEY = '7e8b7c6f7ed3827a5d9d6646a7cb3b57';
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`;
     const request = axios.get(url);
     const response = await request;
+    console.log(response);
+
+    // 5 day forecast using filther method for forecast at 12:00 for 5 days
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${API_KEY}&units=metric`;
+    const forecastRequest = axios.get(forecastUrl);
+    const forecastResponse = await forecastRequest;
+    let arr = forecastResponse.data.list
+    let forecastFiveDays = arr.filter(item => item.dt_txt.includes("12:00")) 
+    console.log(forecastFiveDays)
+
+    // UseState
     setWeather(response.data.main);
     setCity(response.data.name);
+    setForecast(forecastFiveDays)
     setError(null);
   };
-  weather && console.log(weather);
-
-  return (
+  weather && console.log(weather,forecast);
+  
+  return (  
     <div className="main">
       <Header />
       <Content>
-        <DateTime/>
+        <DateTime />
         <Context.Provider value={{ api_call, weather, city }}>
           <WeatherSearch />
           {weather && <WeatherData />}
           {error && <Error error={error} />}
         </Context.Provider>
-        <Footer/>
+        <WeatherIcon />
+        <Footer />
       </Content>
     </div>
   );
 };
 
 export default Main;
+
+// Api key for google location AIzaSyAHBBsw_Et7zrfzkdMuTgUy99NJYsMFQ9s
---
+++ src/components/WeatherIcon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import cloudy from '../assets/img/cloudy.svg'
+
+const WeatherIcon = () => {
+  return (
+    <div className="weather-icon">
+      <h1>Hello</h1>
+      <img src={cloudy} alt=""/>
+    </div>
+  )
+}
+
+export default WeatherIcon
